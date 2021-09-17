013a6adaa09fe49fd378032c92300888a8270fe4
Felipe Polato <fepolato@hotmail.com>
2021-09-17 11:31:33 -0300

    formularios de regirtro com input controlado

--- src/components/FormRegistrs/index.jsx
+++ src/components/FormRegistrs/index.jsx
@@ -1,47 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function FormRegisters() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [pais, setPais] = useState("");
+  const [municipio, setMunicipio] = useState("");
+  const [estado, setEstado] = useState("");
+  const [cep, setCep] = useState("");
+  const [pis, setPis] = useState("");
+  const [senha, setSenha] = useState("");
+
+  console.log(name, email, pais, municipio, estado, cep, pis, senha)
+
+  const hendlerName = (event) => {
+    setName(event.target.value);
+  };
+
+  const hendlerEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const hendlerPais = (event) => {
+    setPais(event.target.value);
+  };
+
+  const hendlerEstado = (event) => {
+    setEstado(event.target.value);
+  };
+
+  const hendlerCep = (event) => {
+    setCep(event.target.value);
+  };
+
+  const hendlerPis = (event) => {
+    setPis(event.target.value);
+  };
+
+  const hendlerMunicipio = (event) => {
+    setMunicipio(event.target.value);
+  };
+
+  const hendlerSenha = (event) => {
+    setSenha(event.target.value);
+  };
+
   return (
     <form type="submit">
       <label>
         Nome
-        <input name="name" type="text" required />
+        <input name="name" type="text" required onChange={hendlerName} />
       </label>
 
       <label>
         E-mail
-        <input name="email" type="email" required />
+        <input name="email" type="email" required onChange={hendlerEmail}/>
       </label>
       <p>Endereço do Usuário</p>
 
       <label>
         País
-        <input name="pais" type="text" required />
+        <input name="pais" type="text" required onChange={hendlerPais}/>
       </label>
 
       <label>
         Município
-        <input name="municiopio" type="text" required />
+        <input name="municiopio" type="text" required onChange={hendlerMunicipio}/>
       </label>
 
       <label>
         Estado
-        <input name="estado" type="text" required />
+        <input name="estado" type="text" required onChange={hendlerEstado}/>
       </label>
 
       <label for="CEP">
         CEP:
-        <input name="CEP" id="CEP" required pattern="\d{5}-\d{3}" />
+        <input name="CEP" id="CEP" required pattern="\d{5}-\d{3}" onChange={hendlerCep}/>
       </label>
 
       <label for="PIS">
         PIS:
-        <input name="PIS" id="PIS" required />
+        <input name="PIS" id="PIS" required onChange={hendlerPis}/>
       </label>
 
       <label>
         Senha:
-        <input name="password" type="password" required />
+        <input name="password" type="password" required onChange={hendlerSenha}/>
       </label>
 
       <button>Enviar</button>
