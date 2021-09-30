7fca0b191654907da04f4b0697120ee7936d7286
Felipe <fepolato@hotmail.com>
2021-09-30 16:25:32 -0300

    limpeza de codigo

--- src/screen/ScreenRegistration/index.jsx
+++ src/screen/ScreenRegistration/index.jsx
@@ -1,18 +1,10 @@
 import React from "react";
 import FormRegisters from "../../components/FormRegistrs";
 
-
-
-///style////////////////////////////////
-// import {  } from "./style";
-
 export default function ScreenRegistration() {
-  
-
   return (
     <div>
       <FormRegisters />
-   
     </div>
   );
 }
